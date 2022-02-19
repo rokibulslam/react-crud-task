a9f59ade9b2d6135e21f3dcf9013dde6b2409986
rokibulslam <rokibulslam6@gmail.com>
2022-02-19 18:56:41 +0600

    create method done

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddUser from './AddUser/AddUser';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <AddUser></AddUser>
+        </div>
+    );
+};
+
+export default Dashboard;
